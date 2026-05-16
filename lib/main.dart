b9c50e7273791ba9d5ec51935369f95b31f83b87
pinaya agustin <agustinpinaya09@gmail.com>
2026-05-16 21:16:25 +0700

    update judul alat

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sistem Peminjaman Alat',
+      title: 'Sistem Peminjaman Alat Laboratorium',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
